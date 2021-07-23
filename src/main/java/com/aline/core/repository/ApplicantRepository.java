6a8631beafe30fda0e4effae8a34dd066aa774e5
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-23 02:14:41 -0600

    Implement specification in ApplicantRepository.

--- src/main/java/com/aline/core/repository/ApplicantRepository.java
+++ src/main/java/com/aline/core/repository/ApplicantRepository.java
@@ -1,11 +1,17 @@
 package com.aline.core.repository;
 
 import com.aline.core.model.Applicant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+public interface ApplicantRepository extends JpaRepository<Applicant, Long>, JpaSpecificationExecutor<Applicant> {
+
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
     boolean existsByDriversLicense(String driversLicense);
