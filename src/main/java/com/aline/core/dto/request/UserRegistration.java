459851b0172f56260b95fdadb6630858f56e1298
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-05 12:21:56 -0600

    AF-236 Add username and password validators to request dtos.

--- src/main/java/com/aline/core/dto/request/UserRegistration.java
+++ src/main/java/com/aline/core/dto/request/UserRegistration.java
@@ -1,9 +1,13 @@
 package com.aline.core.dto.request;
 
+import com.aline.core.validation.annotations.Password;
+import com.aline.core.validation.annotations.Username;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -11,7 +15,11 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class UserRegistration {
 
-
+    @NotBlank(message = "Username is required.")
+    @Username
     private String username;
+
+    @NotBlank(message = "Password is required.")
+    @Password
     private String password;
 }
