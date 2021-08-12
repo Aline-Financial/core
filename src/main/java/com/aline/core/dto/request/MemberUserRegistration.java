48576c49fd4bb82bc2f30306b4051f5aaa90701d
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-12 11:37:13 -0600

    Optimize imports.

--- src/main/java/com/aline/core/dto/request/MemberUserRegistration.java
+++ src/main/java/com/aline/core/dto/request/MemberUserRegistration.java
@@ -7,7 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
