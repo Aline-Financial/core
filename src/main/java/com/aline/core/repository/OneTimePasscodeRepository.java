5b4b0f88092a6a44e9175b39200a134215480435
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-23 02:06:12 -0600

    Add delete by id query in OTP repo.

--- src/main/java/com/aline/core/repository/OneTimePasscodeRepository.java
+++ src/main/java/com/aline/core/repository/OneTimePasscodeRepository.java
@@ -3,8 +3,11 @@ package com.aline.core.repository;
 import com.aline.core.model.OneTimePasscode;
 import com.aline.core.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +15,10 @@ public interface OneTimePasscodeRepository extends JpaRepository<OneTimePasscode
 
     Optional<OneTimePasscode> findByOtp(String otp);
     Optional<OneTimePasscode> findByUser(User user);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM OneTimePasscode u WHERE u.user.id = ?1")
+    void deleteByUserId(long id);
 
 }
