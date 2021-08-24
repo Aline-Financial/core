c1aaafa26f9d8000a008c3d6bd41aabe46dce248
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-24 11:13:28 -0600

    AF-258 Add JWT packages to POM

---
+++ src/main/java/com/aline/core/config/security/JwtSecretKey.java
@@ -0,0 +1,27 @@
+package com.aline.core.config.security;
+
+import com.aline.core.config.AppConfig;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtSecretKey {
+
+    private final AppConfig appConfig;
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(appConfig
+                .getSecurity()
+                .getJwt()
+                .getSecretKey()
+                .getBytes(StandardCharsets.UTF_8));
+    }
+
+}
