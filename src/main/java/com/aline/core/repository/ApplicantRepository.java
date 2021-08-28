641b44a7c998dc088c3927b997edd1b31c9c41f0
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-27 20:49:56 -0600

    AF-258 Add get applicant by username method to repo.

--- src/main/java/com/aline/core/repository/ApplicantRepository.java
+++ src/main/java/com/aline/core/repository/ApplicantRepository.java
@@ -1,7 +1,10 @@
 package com.aline.core.repository;
 
 import com.aline.core.model.Applicant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ApplicantRepository extends JpaRepositoryWithSpecification<Applicant, Long> {
@@ -10,4 +13,7 @@ public interface ApplicantRepository extends JpaRepositoryWithSpecification<Appl
     boolean existsByPhone(String phone);
     boolean existsByDriversLicense(String driversLicense);
     boolean existsBySocialSecurity(String socialSecurity);
+
+    @Query("SELECT a FROM Applicant a LEFT JOIN MemberUser u ON u.member.applicant.id = a.id WHERE u.username = ?1")
+    Optional<Applicant> findApplicantByUsername(String username);
 }
