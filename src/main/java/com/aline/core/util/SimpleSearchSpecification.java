7b9b637312a3791dac6d0ab5870c5908f705fb2e
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-28 11:40:36 -0600

    AF-258 Abstract search specification to allow for more complex searches.

--- src/main/java/com/aline/core/util/SimpleSearchSpecification.java
+++ src/main/java/com/aline/core/util/SimpleSearchSpecification.java
@@ -1,5 +1,6 @@
 package com.aline.core.util;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -10,13 +11,22 @@ import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.Attribute;
 import java.util.Arrays;
 
+/**
+ * The SearchSpecification class is used to return
+ * a specified list of elements based on the search term.
+ * The search term searches through all columns of the entity
+ * that are strings and if at least one term is matched in any
+ * of the string columns, it will return a list with that entity
+ * in it.
+ * @param <T> The entity type that a search is being applied to.
+ */
 @RequiredArgsConstructor
-public abstract class SearchSpecification<T> implements Specification<T> {
+public class SimpleSearchSpecification<T> implements Specification<T> {
 
     private final String search;
 
     @Override
-    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
         String[] searchTerms = search.split("[\\s,]");
 
         Predicate[] predicates = Arrays.stream(searchTerms)
