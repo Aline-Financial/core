0b08a7fb79eab350305a96e950a28dd682125260
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-27 23:59:30 -0600

    AF-258 Add documentation. Add userIsManagement method.

--- src/main/java/com/aline/core/security/service/AbstractAuthorizationService.java
+++ src/main/java/com/aline/core/security/service/AbstractAuthorizationService.java
@@ -9,21 +9,46 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * This class acts a sort of predicate to be used with
+ * the pre/post authorize annotations. It provides methods
+ * for easy access to the security context information.
+ * @param <T> The parameter type of the predicate.
+ */
 public abstract class AbstractAuthorizationService<T> {
 
+    /**
+     * Get the current security context.
+     * @return The current stateless security context.
+     */
     protected SecurityContext getSecurityContext() {
         return SecurityContextHolder.getContext();
     }
 
+    /**
+     * Get the current authentication object.
+     * @return Authentication object from the security context.
+     */
     protected Authentication getAuthentication() {
         return getSecurityContext().getAuthentication();
     }
 
+    /**
+     * Get the current user's username.
+     * @return A string of the current username.
+     */
     protected String getUsername() {
         return getAuthentication().getName();
     }
 
+    /**
+     * Get the current user's role as an authority.
+     * @return UserRoleAuthority representing the UserRole.
+     * @see UserRole
+     */
     protected UserRoleAuthority getAuthority() {
         GrantedAuthority grantedAuthority = new ArrayList<>(getAuthentication()
                 .getAuthorities())
@@ -34,8 +59,21 @@ public abstract class AbstractAuthorizationService<T> {
         return (UserRoleAuthority) grantedAuthority ;
     }
 
+    /**
+     * Get the current user's role.
+     * @return UserRole enum
+     */
     protected UserRole getRole() {
         return getAuthority().getUserRole();
+    }
+
+    protected boolean userIsManagement() {
+        List<UserRole> managementRoles = Arrays.asList(
+                UserRole.EMPLOYEE,
+                UserRole.ADMINISTRATOR
+        );
+
+        return managementRoles.contains(getRole());
     }
 
     public abstract boolean canAccess(T returnObject);
