646d89e4c05ff72e4e0bbb64559a385d2c2581d0
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-28 16:27:29 -0600

    AF-258 Add web security checker message.

--- src/main/java/com/aline/core/security/WebSecurityChecker.java
+++ src/main/java/com/aline/core/security/WebSecurityChecker.java
@@ -2,16 +2,16 @@ package com.aline.core.security;
 
 import com.aline.core.annotation.ConditionalOnMissingWebSecurity;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @ConditionalOnMissingWebSecurity
 @Slf4j(topic = "Web Security Checker")
 @Component
-public class WebSecurityChecker implements ApplicationListener<ApplicationReadyEvent> {
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        log.warn("Web security is ENABLED but no implementation of the AbstractWebSecurityConfig was found.");
+public class WebSecurityChecker {
+    @PostConstruct
+    public void init() {
+        log.warn("Web security is ENABLED but no implementation of the AbstractWebSecurityConfig was found. Create a configuration that is annotated with @WebSecurityConfiguration and extends AbstractWebSecurityConfig or set app.security.disable-web-security to true to disable this warning.");
     }
 }
