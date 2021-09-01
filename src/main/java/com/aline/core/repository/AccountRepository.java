58448b2b90e4dba367d50e72fb2d795658a909ac
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-31 20:44:22 -0600

    AF-256 Update find all by member id method.

--- src/main/java/com/aline/core/repository/AccountRepository.java
+++ src/main/java/com/aline/core/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.aline.core.repository;
 
 import com.aline.core.model.account.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,7 +14,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     @Query("SELECT a FROM Account a " +
             "JOIN a.members m " +
-            "WHERE m.membershipId = ?1")
-    List<Account> findAccountsByMembershipNumber(String membershipNumber);
+            "WHERE m.id = ?1")
+    Page<Account> findAllByMemberId(long memberId, Pageable pageable);
 
 }
