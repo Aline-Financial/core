a6144b676e3dd23070924b73d11ecc0d46932791
beki01 <rebeca.gonzalez@smoothstack.com>
2021-09-29 13:00:30 -0700

    Update core ref and dependancy for meta models

--- src/main/java/com/aline/core/repository/MemberRepository.java
+++ src/main/java/com/aline/core/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package com.aline.core.repository;
 
 import com.aline.core.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecificationExecutor<Member> {
     Optional<Member> findByMembershipId(String membershipId);
 }
